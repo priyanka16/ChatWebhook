e8c837a10a9aaa11e90f87f251e41842cd4abcb1
pvernekar <pvernekar@deloitte.com>
2017-09-20 16:23:42 +0530

    removed dateutil

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, redirect, abort, make_response
 from datetime import datetime,date,timedelta
-from dateutil import relativedelta
 from collections import OrderedDict
 from random import randint
 import json
